1c4698b8b060658b56827a5d26ca520f15a19290
Unknown <seckndanane16@gmail.com>
2018-07-14 21:55:13 +0100

    Nouvelle Page d'Accueil avec OCMS
    Réalisée avec Momo & GC7

---
+++ ac7/pageOrigineC57.php
@@ -0,0 +1,167 @@
+<!--
+TAF prioritaires
+ - Lier slack avec les dépôts c57
+ - Images/Icônes numéro des étapes
+ (1 ce site (Edit On line / [VM], GitHub, outils /localhost/Fork-MR-Fetch / ...) - 2 Laravel - 3 Bestmomo - 4 oCMS - 5 C2B)
+ - Pose d'un oCMS pour C57
+ - Sur les numéros, créer lien sur pages blog pour chaque étape alors décrite de A à Z
+ - CSS => Utiliser vraiment less
+L'allure devra être comme celle-ci
+- Ajouter Lien: Vous y gagner d'emblée (Compétences, trucs & astuces, temps, etc..) Mais... Et si en plus, cela vous rapportait directement...?
+
+TAF secondaires
+Lien dépît c57 - No mouvements
+-->
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Accueil | c57.fr</title>
+    <meta charset=UTF-8"/>
+    <meta name="description" content="Page d'accueil de c57.fr"/>
+    <link rel="stylesheet" href="assets/css/style.min.css">
+</head>
+
+<body>
+<!--  Commenter/décommenter pour afficher un message dans le haut de la page-->
+<div class="lio">
+    <!--  <mark>À noter: Tests en cours...</mark>-->
+</div>
+
+<section>
+
+            <div class="adroite top-right links">
+                            <a href="https://c57.fr/login">Connexion</a>
+                <a href="https://c57.fr/register">S'enregistrer</a>
+                    </div>
+    
+</section>
+
+<h1>
+    C57.fr,
+</h1>
+
+<p>LA réponse.</p>
+
+<br/>
+<hr>
+
+<h3>Apprenez par l'action ! <a href="https://github.com/c57fr/l5" title=" LE dépôt GitHub, uniquement pour dev local de chacun...
+
+ Rappel: Après avoir 'forké'... ">
+        1
+        <mark>Dépôt GitHub L5 *</mark>
+    </a></h3>
+<i>* Installation en 2 lignes de commandes !</i>
+<hr>
+<div class="resserre">
+    Tu doutes, tu as peut-être peur, tu ne sais même pas pas si cela te concerne vraiment, tu te demandes même ce que tu
+    y
+    gagneras....? Une seule solution pour clarifier tout cela: <a href="https://c57.fr/register"
+                                                                  class="ext c57">INSCRIS-TOI !</a> Et
+    quoiqu'il advienne, ne retiens au besoin,
+    là aussi, qu'une seule chose...: Ce lien:
+    <h4><a href="faq" title="Avant, merci de lire notre page Questions Fréquentes...">Besoin d'aide? Une question? Une
+            attente ou une requête?</a></h4>
+
+    <p>Et ne tkt pas plus que cela, car là est ta place :-) !</p>
+</div>
+
+<hr>
+
+<p class="etapes">À voir aussi:
+    <a href="https://github.com/C57fr/pooga" target="_blank">2&nbsp;Pooga</a> |
+    <a href="https://github.com/C57fr/Do" target="_blank">3&nbsp;Do</a> |
+    <a href="https://github.com/c57fr/c57" title="Oui, ce site, dont tu peux co-écrire chaque page...">4&nbsp;c57</a> |
+    <a href="http://bestmomo.c57.fr" class="ext" target="_blank">5&nbsp;Laravel (Customisé/BestMomo)</a> |
+    <a href="http://voyager.c57.fr" class="ext" target="_blank">6&nbsp;Voyager</a> |
+    <a href="http://october.c57.fr" class="ext" target="_blank">7&nbsp;OctoberCMS</a> |
+    <a href="https://chemin2bonheur.com/be/backend/auth/signin" class="
+    ext" target="_blank">8&nbsp;C2B</a>
+</p>
+
+<h4><em class="noCadre">Le Dev Informatique, c'est maintenant et enfin pour tout le monde...<br/>
+        ... Et en plus, avec la puissance des <a href="https://fr.wikipedia.org/wiki/Open_source"
+                                                 target="_blank">O.S.</a><br/>
+        <span class="c57">C</span>'est en <span class="c57">5</span> - <span class="c57">7</span>..<span
+                class="c57">.</span>
+        Rapide,facile, même pour les 'pures' <span class="c57">fr</span>ancophones !</em></h4>
+<hr>
+BRAVOS: Derniers inscrits: BestMomo, XRumer, Doro W., Richard M.
+
+
+<hr/>
+<!-- Désactivation Stack pour le moment... La FAQ doit se remplir...!
+<div style="text-align: center" class="sp1">
+  <a href="http://c57.slack.com" class="ext" target="_blank">Connection (Slack)</a>
+</div>
+<hr/>
+-->
+<div style="text-align: right; margin-top: -.125%;" class="sp1">
+    &copy; 2017 - Propulsé par Laravel et <strong><a href="https://github.com/c57fr/c57"
+                                                     title="Dépôt GitHub">C57</a></strong> -
+    <!--TAF C57 => Lien login du oCMS pr C57-->
+    <em><a href="http://sg1.COTE7.com" class="ext" target="_blank">GC7</a></em>
+</div>
+</div>
+
+<script>
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-61289532-1', 'auto');
+    ga('send', 'pageview');
+</script>
+
+<link rel='stylesheet' type='text/css' property='stylesheet' href='//c57.fr/_debugbar/assets/stylesheets?v=1521687188'><script type='text/javascript' src='//c57.fr/_debugbar/assets/javascript?v=1521687192'></script><script type="text/javascript">jQuery.noConflict(true);</script>
+<script type="text/javascript">
+var phpdebugbar = new PhpDebugBar.DebugBar();
+phpdebugbar.addTab("messages", new PhpDebugBar.DebugBar.Tab({"icon":"list-alt","title":"Messages", "widget": new PhpDebugBar.Widgets.MessagesWidget()}));
+phpdebugbar.addIndicator("time", new PhpDebugBar.DebugBar.Indicator({"icon":"clock-o","tooltip":"Request Duration"}), "right");
+phpdebugbar.addTab("timeline", new PhpDebugBar.DebugBar.Tab({"icon":"tasks","title":"Timeline", "widget": new PhpDebugBar.Widgets.TimelineWidget()}));
+phpdebugbar.addIndicator("memory", new PhpDebugBar.DebugBar.Indicator({"icon":"cogs","tooltip":"Memory Usage"}), "right");
+phpdebugbar.addTab("exceptions", new PhpDebugBar.DebugBar.Tab({"icon":"bug","title":"Exceptions", "widget": new PhpDebugBar.Widgets.ExceptionsWidget()}));
+phpdebugbar.addTab("views", new PhpDebugBar.DebugBar.Tab({"icon":"leaf","title":"Views", "widget": new PhpDebugBar.Widgets.TemplatesWidget()}));
+phpdebugbar.addTab("route", new PhpDebugBar.DebugBar.Tab({"icon":"share","title":"Route", "widget": new PhpDebugBar.Widgets.VariableListWidget()}));
+phpdebugbar.addIndicator("currentroute", new PhpDebugBar.DebugBar.Indicator({"icon":"share","tooltip":"Route"}), "right");
+phpdebugbar.addTab("queries", new PhpDebugBar.DebugBar.Tab({"icon":"inbox","title":"Queries", "widget": new PhpDebugBar.Widgets.SQLQueriesWidget()}));
+phpdebugbar.addTab("emails", new PhpDebugBar.DebugBar.Tab({"icon":"inbox","title":"Mails", "widget": new PhpDebugBar.Widgets.MailsWidget()}));
+phpdebugbar.addTab("session", new PhpDebugBar.DebugBar.Tab({"icon":"archive","title":"Session", "widget": new PhpDebugBar.Widgets.VariableListWidget()}));
+phpdebugbar.addTab("request", new PhpDebugBar.DebugBar.Tab({"icon":"tags","title":"Request", "widget": new PhpDebugBar.Widgets.VariableListWidget()}));
+phpdebugbar.setDataMap({
+"messages": ["messages.messages", []],
+"messages:badge": ["messages.count", null],
+"time": ["time.duration_str", '0ms'],
+"timeline": ["time", {}],
+"memory": ["memory.peak_usage_str", '0B'],
+"exceptions": ["exceptions.exceptions", []],
+"exceptions:badge": ["exceptions.count", null],
+"views": ["views", []],
+"views:badge": ["views.nb_templates", 0],
+"route": ["route", {}],
+"currentroute": ["route.uri", ],
+"queries": ["queries", []],
+"queries:badge": ["queries.nb_statements", 0],
+"emails": ["swiftmailer_mails.mails", []],
+"emails:badge": ["swiftmailer_mails.count", null],
+"session": ["session", {}],
+"request": ["request", {}]
+});
+phpdebugbar.restoreState();
+phpdebugbar.ajaxHandler = new PhpDebugBar.AjaxHandler(phpdebugbar);
+phpdebugbar.ajaxHandler.bindToXHR();
+phpdebugbar.setOpenHandler(new PhpDebugBar.OpenHandler({"url":"https:\/\/c57.fr\/_debugbar\/open"}));
+phpdebugbar.addDataSet({"__meta":{"id":"ce7ec30591e794caf938e435b94c5ac7","datetime":"2018-07-14 22:14:49","utime":1531599289.816774,"method":"GET","uri":"\/","ip":"160.178.30.204"},"php":{"version":"7.2.4-1+0~20180405085422.20+stretch~1.gbpbff9f0","interface":"apache2handler"},"messages":{"count":1,"messages":[{"message":"Bienvenue dans la zone de dev francophone, autour de Laravel, et autres...","is_string":true,"label":"info","time":1531599289.813619}]},"time":{"start":1531599289.756,"end":1531599289.816799,"duration":0.06079888343811035,"duration_str":"60.8ms","measures":[{"label":"Booting","start":1531599289.756,"relative_start":0,"end":1531599289.805188,"relative_end":1531599289.805188,"duration":0.04918789863586426,"duration_str":"49.19ms","params":[],"collector":null},{"label":"Application","start":1531599289.799407,"relative_start":0.04340696334838867,"end":1531599289.816801,"relative_end":2.1457672119140625e-6,"duration":0.017394065856933594,"duration_str":"17.39ms","params":[],"collector":null}]},"memory":{"peak_usage":4194304,"peak_usage_str":"4MB"},"exceptions":{"count":0,"exceptions":[]},"views":{"nb_templates":1,"templates":[{"name":"pages.welcome (resources\/views\/pages\/welcome.blade.php)","param_count":0,"params":[],"type":"blade"}]},"route":{"uri":"GET \/","middleware":"web","uses":"Closure {#1099\n  class: \"Illuminate\\Routing\\Router\"\n  this: Illuminate\\Routing\\Router {#28 \u2026}\n  file: \"\/home\/admin\/web\/c57.fr\/public_html\/routes\/web.php\"\n  line: \"18 to 22\"\n}","namespace":"App\\Http\\Controllers","prefix":null,"where":[],"file":"routes\/web.php:18-22"},"queries":{"nb_statements":0,"nb_failed_statements":0,"accumulated_duration":0,"accumulated_duration_str":"0\u03bcs","statements":[]},"swiftmailer_mails":{"count":0,"mails":[]},"session":{"_token":"xatW0qa9BskKTURMbCleWt2lX7LUzxLjD9q92SAF","_previous":"array:1 [\n  \"url\" => \"https:\/\/c57.fr\"\n]","PHPDEBUGBAR_STACK_DATA":"[]","_flash":"array:2 [\n  \"old\" => []\n  \"new\" => []\n]"},"request":{"format":"html","content_type":"text\/html; charset=UTF-8","status_text":"OK","status_code":"200","request_query":"[]","request_request":"[]","request_headers":"array:8 [\n  \"host\" => array:1 [\n    0 => \"c57.fr\"\n  ]\n  \"user-agent\" => array:1 [\n    0 => \"Mozilla\/5.0 (X11; Ubuntu; Linux x86_64; rv:60.0) Gecko\/20100101 Firefox\/60.0\"\n  ]\n  \"accept\" => array:1 [\n    0 => \"text\/html,application\/xhtml+xml,application\/xml;q=0.9,*\/*;q=0.8\"\n  ]\n  \"accept-language\" => array:1 [\n    0 => \"en-GB,en;q=0.5\"\n  ]\n  \"accept-encoding\" => array:1 [\n    0 => \"gzip, deflate, br\"\n  ]\n  \"cookie\" => array:1 [\n    0 => \"_ga=GA1.2.597310397.1523840862; XSRF-TOKEN=eyJpdiI6IjVScUZaTStaNTRBRGZiN2xxTWZENlE9PSIsInZhbHVlIjoiNzEwNnJqdmNGeXdcL3VyZm5lTDl6Q2pSRHFFOVAyN1U4azhaTmFFRVEwcmJHUjltQlRnbmRLdXJWVlU3dFZsaEk5U2F0TGxYZExuVTNtOVU5UzBpeG1BPT0iLCJtYWMiOiJjNDQxYmZmN2E3N2VlODNjZDMwMTVhZDM0MzhiYzc2ZjczYjhhMDcyYmRkZmUxM2FlNzMyMGI1NTc0NTRjZmEzIn0%3D; laravel_session=eyJpdiI6IktSd3hmSlltT1g0aDZIdkUzRTNzSHc9PSIsInZhbHVlIjoiNkhGSlZpMkJldEtualBuS0FcLytKTklMV0JVU1dSaHFMazE2QVhPUFRTbFZibTBBZll0UUFocnpBUW1MS1lJaGxUbTlSNlZ5eWNXb2JiajRwVkJvbTRnPT0iLCJtYWMiOiI0YzJiN2UzMzk1NDA3NGM2NmE1NTFkMTZkZDVhMjU2YzI4ODI3NDNkM2E1MTBlYWNiNTdiNmQxZmE5NDc5ODAyIn0%3D; _gid=GA1.2.1813951076.1531592138\"\n  ]\n  \"connection\" => array:1 [\n    0 => \"keep-alive\"\n  ]\n  \"upgrade-insecure-requests\" => array:1 [\n    0 => \"1\"\n  ]\n]","request_server":"array:37 [\n  \"REDIRECT_HTTPS\" => \"on\"\n  \"REDIRECT_SSL_TLS_SNI\" => \"c57.fr\"\n  \"REDIRECT_STATUS\" => \"200\"\n  \"HTTPS\" => \"on\"\n  \"SSL_TLS_SNI\" => \"c57.fr\"\n  \"HTTP_HOST\" => \"c57.fr\"\n  \"HTTP_USER_AGENT\" => \"Mozilla\/5.0 (X11; Ubuntu; Linux x86_64; rv:60.0) Gecko\/20100101 Firefox\/60.0\"\n  \"HTTP_ACCEPT\" => \"text\/html,application\/xhtml+xml,application\/xml;q=0.9,*\/*;q=0.8\"\n  \"HTTP_ACCEPT_LANGUAGE\" => \"en-GB,en;q=0.5\"\n  \"HTTP_ACCEPT_ENCODING\" => \"gzip, deflate, br\"\n  \"HTTP_COOKIE\" => \"_ga=GA1.2.597310397.1523840862; XSRF-TOKEN=eyJpdiI6IjVScUZaTStaNTRBRGZiN2xxTWZENlE9PSIsInZhbHVlIjoiNzEwNnJqdmNGeXdcL3VyZm5lTDl6Q2pSRHFFOVAyN1U4azhaTmFFRVEwcmJHUjltQlRnbmRLdXJWVlU3dFZsaEk5U2F0TGxYZExuVTNtOVU5UzBpeG1BPT0iLCJtYWMiOiJjNDQxYmZmN2E3N2VlODNjZDMwMTVhZDM0MzhiYzc2ZjczYjhhMDcyYmRkZmUxM2FlNzMyMGI1NTc0NTRjZmEzIn0%3D; laravel_session=eyJpdiI6IktSd3hmSlltT1g0aDZIdkUzRTNzSHc9PSIsInZhbHVlIjoiNkhGSlZpMkJldEtualBuS0FcLytKTklMV0JVU1dSaHFMazE2QVhPUFRTbFZibTBBZll0UUFocnpBUW1MS1lJaGxUbTlSNlZ5eWNXb2JiajRwVkJvbTRnPT0iLCJtYWMiOiI0YzJiN2UzMzk1NDA3NGM2NmE1NTFkMTZkZDVhMjU2YzI4ODI3NDNkM2E1MTBlYWNiNTdiNmQxZmE5NDc5ODAyIn0%3D; _gid=GA1.2.1813951076.1531592138\"\n  \"HTTP_CONNECTION\" => \"keep-alive\"\n  \"HTTP_UPGRADE_INSECURE_REQUESTS\" => \"1\"\n  \"PATH\" => \"\/usr\/local\/sbin:\/usr\/local\/bin:\/usr\/sbin:\/usr\/bin:\/sbin:\/bin\"\n  \"SERVER_SIGNATURE\" => \"\"\n  \"SERVER_SOFTWARE\" => \"Apache\/2.4.25 (Debian) mod_fcgid\/2.3.9 OpenSSL\/1.0.2l\"\n  \"SERVER_NAME\" => \"c57.fr\"\n  \"SERVER_ADDR\" => \"144.217.92.216\"\n  \"SERVER_PORT\" => \"443\"\n  \"REMOTE_ADDR\" => \"160.178.30.204\"\n  \"DOCUMENT_ROOT\" => \"\/home\/admin\/web\/c57.fr\/public_html\"\n  \"REQUEST_SCHEME\" => \"https\"\n  \"CONTEXT_PREFIX\" => \"\"\n  \"CONTEXT_DOCUMENT_ROOT\" => \"\/home\/admin\/web\/c57.fr\/public_html\"\n  \"SERVER_ADMIN\" => \"info@c57.fr\"\n  \"SCRIPT_FILENAME\" => \"\/home\/admin\/web\/c57.fr\/public_html\/public\/index.php\"\n  \"REMOTE_PORT\" => \"37716\"\n  \"REDIRECT_URL\" => \"\/\"\n  \"GATEWAY_INTERFACE\" => \"CGI\/1.1\"\n  \"SERVER_PROTOCOL\" => \"HTTP\/1.1\"\n  \"REQUEST_METHOD\" => \"GET\"\n  \"QUERY_STRING\" => \"\"\n  \"REQUEST_URI\" => \"\/\"\n  \"SCRIPT_NAME\" => \"\/public\/index.php\"\n  \"PHP_SELF\" => \"\/public\/index.php\"\n  \"REQUEST_TIME_FLOAT\" => 1531599289.756\n  \"REQUEST_TIME\" => 1531599289\n]","request_cookies":"array:4 [\n  \"_ga\" => null\n  \"XSRF-TOKEN\" => \"xatW0qa9BskKTURMbCleWt2lX7LUzxLjD9q92SAF\"\n  \"laravel_session\" => \"iwJN7YPCsk1cJlNxRSP47APUX0025ZkcSUSdacok\"\n  \"_gid\" => null\n]","response_headers":"array:4 [\n  \"cache-control\" => array:1 [\n    0 => \"no-cache, private\"\n  ]\n  \"date\" => array:1 [\n    0 => \"Sat, 14 Jul 2018 20:14:49 GMT\"\n  ]\n  \"content-type\" => array:1 [\n    0 => \"text\/html; charset=UTF-8\"\n  ]\n  \"Set-Cookie\" => array:2 [\n    0 => \"XSRF-TOKEN=eyJpdiI6Ikh1RUxET08rV3lId0ZKajVmUjRZaWc9PSIsInZhbHVlIjoielhXQ0VXNTJwV2dPUVFJVG44aVZBdkdSTStIZ0FybTlxOFNxbXQrT0tubEZFMk1IQjZ6RTBVb292VHhlQjBcL2dSMnI0YnlrNmY2UzVvN1VQbmtkYThBPT0iLCJtYWMiOiI5ZDBjMzc0ZmMxZmVmYjJmZDI3ZThmZjcwOWUwYzVkMzFkOGJkZWY0MjE5ZDZjY2U1MDY4OGUyYWRhODk5OGE5In0%3D; expires=Sat, 14-Jul-2018 22:14:49 GMT; path=\/\"\n    1 => \"laravel_session=eyJpdiI6IjlLTzFHdTdiYURDaGp0K01TQlgxSUE9PSIsInZhbHVlIjoiSENcL1hYYm5yWG5QYnA2OGlHVkZ4RTM4cW9ZUHU5Q0ZHNGtzOVpqMHpUeWVJUzlpanREeTFhb0pHcDlLb01wZWZiOUVsZjZhNGVpQUZOU0pKRFwvNm51dz09IiwibWFjIjoiNGYxODIwNzcxMTc4ZmRlNWM1YjdiZmViYjY0MjIyZWZlNzk5MDYzNWNiOThkNjk5NjM0MTkyMzQ2ZWY4OTBlZSJ9; expires=Sat, 14-Jul-2018 22:14:49 GMT; path=\/; httponly\"\n  ]\n]","path_info":"\/","session_attributes":"array:4 [\n  \"_token\" => \"xatW0qa9BskKTURMbCleWt2lX7LUzxLjD9q92SAF\"\n  \"_previous\" => array:1 [\n    \"url\" => \"https:\/\/c57.fr\"\n  ]\n  \"PHPDEBUGBAR_STACK_DATA\" => []\n  \"_flash\" => array:2 [\n    \"old\" => []\n    \"new\" => []\n  ]\n]"}}, "ce7ec30591e794caf938e435b94c5ac7");
+
+</script>
+</body>
+</html>
